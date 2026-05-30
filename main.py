8f63d8201bdc4e6758e4a92a2194aa95ae281dd3
Артур <artalana196@gmail.com>
2026-05-31 01:13:40 +0300

    Return to Cutlly

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Config.set('graphics', 'height', '550')
 
 import os, sys
 import threading
-import pyshorteners
+import requests
+import configparser
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.clipboard import Clipboard
@@ -17,7 +18,6 @@ def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
-
 resource_add_path(resource_path('.'))
 
 class MainLayout(BoxLayout):
@@ -26,6 +26,8 @@ class MainLayout(BoxLayout):
 class ShortenerApp(App):
     def build(self):
         self.title = "URL Shortener"
+        self.config_file = 'config.ini'
+        self.config = configparser.ConfigParser()
         self.spinner_anim = None
         return MainLayout()
     
@@ -40,7 +42,7 @@ class ShortenerApp(App):
 
     def start_spinner(self):
         lbl = self.root.ids.result_label
-        lbl.text = "<|>" 
+        lbl.text = "->" 
         lbl.font_size = '50sp'
         lbl.color = (0.2, 0.6, 1, 1)
         
@@ -69,20 +71,32 @@ class ShortenerApp(App):
         GREEN = (0.3, 0.9, 0.4, 1)
         ORANGE = (1, 0.7, 0.4, 1)
 
+        try:
+            self.config.read(self.config_file)
+            api_key = self.config["cuttly"]["API_KEY"]
+        except Exception:
+            self._update_ui("\nОшибка:\nПроверь config.ini", RED)
+            return
+
         if not long_url:
             self._update_ui("\nВнимание:\nПоле пустое", ORANGE)
             return
+        
+        api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={long_url}"
 
         try:
-            shortener = pyshorteners.Shortener()
-            short_link = shortener.tinyurl.short(long_url)
+            response = requests.get(api_url, timeout=10)
+            data = response.json()
 
-            Clipboard.copy(short_link)
-            self._update_ui(f"Готово!\n[b]{short_link}[/b]\n(Скопировано)", GREEN)
-
-        except Exception as e:
-            print(f"Error: {e}")
-            self._update_ui("Ошибка сети или\nневерная ссылка", RED)
+            if data['url']['status'] == 7:
+                short_link = data['url']['shortLink']
+                Clipboard.copy(short_link)
+                self._update_ui(f"Готово!\n[b]{short_link}[/b]\n(Скопировано)", GREEN)
+            else:
+                self._update_ui(f"Ошибка Cuttly:\nСтатус {data['url']['status']}", RED)
+         
+        except Exception:
+            self._update_ui("Ошибка сети", RED)
 
     def _update_ui(self, text, color):
         Clock.schedule_once(lambda dt: self.stop_spinner(text, color))
